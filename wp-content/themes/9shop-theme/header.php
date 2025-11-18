9d8f45452f3b135254bcf206b6b32e7e228e1576
nhutnguyen1198 <nm11nhut@gmail.com>
2025-11-18 23:39:50 +0700

    commit

--- wp-content/themes/9shop-theme/header.php
+++ wp-content/themes/9shop-theme/header.php
@@ -212,7 +212,7 @@
                                     <a href="404.html" class="dropdown-item">404 Page</a>
                                 </div>
                             </div>
-                            <a href="contact.html" class="nav-item nav-link me-2">Contact</a>
+                            <a href="<?php echo esc_url( get_permalink( get_page_by_path( 'contact-us' ) ) ); ?>" class="nav-item nav-link me-2">Contact</a>
                             <div class="nav-item dropdown d-block d-lg-none mb-3">
                                 <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">All Category</a>
                                 <div class="dropdown-menu m-0">
