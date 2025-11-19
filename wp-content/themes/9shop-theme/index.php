e0f7192f3f936eee333ffffc9305cbb5b9453437
huyvo79 <vohaanhhuy79@gmail.com>
2025-11-19 10:47:33 +0700

    all

--- wp-content/themes/9shop-theme/index.php
+++ wp-content/themes/9shop-theme/index.php
@@ -7,7 +7,7 @@
 <?php get_template_part('template-parts/our-products'); ?>
 <?php get_template_part('template-parts/all-product-items'); ?>
 <?php get_template_part('template-parts/bestSeller'); ?>
-
+<?php echo do_shortcode('[the-post-grid id="197" title=""]'); ?>
 </main>
 
 <?php get_footer(); ?>
